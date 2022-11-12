834acc6c6f8545b341ff3e2dae6394842000ae00
B Kharel <110122254+kharelbuddha@users.noreply.github.com>
2022-11-12 12:26:07 -0600

    branch added

---
+++ .nuxt/components/index.js
@@ -0,0 +1,38 @@
+export { default as Carousel } from '../../components/Carousel.vue'
+export { default as Footer } from '../../components/Footer.vue'
+export { default as Navbar } from '../../components/Navbar.vue'
+export { default as Page } from '../../components/Page.vue'
+export { default as Videobackground } from '../../components/Videobackground.vue'
+export { default as Login } from '../../components/login.vue'
+export { default as Social } from '../../components/social.vue'
+
+// nuxt/nuxt.js#8607
+function wrapFunctional(options) {
+  if (!options || !options.functional) {
+    return options
+  }
+
+  const propKeys = Array.isArray(options.props) ? options.props : Object.keys(options.props || {})
+
+  return {
+    render(h) {
+      const attrs = {}
+      const props = {}
+
+      for (const key in this.$attrs) {
+        if (propKeys.includes(key)) {
+          props[key] = this.$attrs[key]
+        } else {
+          attrs[key] = this.$attrs[key]
+        }
+      }
+
+      return h(options, {
+        on: this.$listeners,
+        attrs,
+        props,
+        scopedSlots: this.$scopedSlots,
+      }, this.$slots.default)
+    }
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+
+<Navbar/>
+
+<login/>
+<Carousel />
+
+<Page/>
+
+<Videobackground/>
+<social/>
+<Footer/>
+
+
+</div>
+</template>
+
+<script>
+import loginVue from '../components/login.vue';
+
+
+  export default {
+    loginVue
+   
+    
+  }
+</script>
+}
+<style scoped>
+
+</style>
